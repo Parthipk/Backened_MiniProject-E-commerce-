6b98b9823a1a20b7bd2e11d401729fa649d98bb8
Parthip.k <145518765+Parthipk@users.noreply.github.com>
2024-12-12 21:28:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const ConnectDB = require("./db_connections/mongo");  // Make sure this path is correct
-const router = require("./router");  // Import the main router
+const ConnectDB = require("./db_connections/mongo");
+const router = require("./router"); 
 const cookieParser = require('cookie-parser');
 const app = express();
 
@@ -8,13 +8,10 @@ app.use(cookieParser());
 const port = 3000;
 require('dotenv').config();
 
-// Connect to DB
 ConnectDB();
 
-// Middleware to parse JSON request bodies
-app.use(express.json());  // This is crucial for POST requests to work
+app.use(express.json());
 
-// Use the routes from router.js
 app.use('/', router);
 
 app.listen(port, () => {
